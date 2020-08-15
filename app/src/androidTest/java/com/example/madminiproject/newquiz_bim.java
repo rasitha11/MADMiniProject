21c5316a2ec23d63a82ba7e97f698a2dca2364c9
UltravioletDarling <iambimsara@gmail.com>
2020-08-15 22:25:42 +0530

    Methods to store inputs Added Publish and Add question options Upgraded UI design of new quiz

--- app/src/androidTest/java/com/example/madminiproject/newquiz_bim.java
+++ app/src/androidTest/java/com/example/madminiproject/newquiz_bim.java
@@ -7,6 +7,8 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,10 +23,52 @@ public class newquiz_bim extends AppCompatActivity {
     private TabItem tab1, tab2, tab3, tab4;
     public  PagerAdapter pagerAdapter;
 
+    String quiztitle,question,answer1,answer2,answer3,answer4;
+
+    EditText titleInput;
+    EditText questionInput;
+    EditText answer1Input;
+    EditText answer2Input;
+    EditText answer3Input;
+    EditText answer4Input;
+
+    Button addquestionButton;
+    Button publishButton;
+
+
+
+    //https://www.youtube.com/watch?v=V0AETAjxqLI
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newquiz_bim);
+
+        titleInput = (EditText) findViewById(R.id.quiztitle);
+        questionInput = (EditText) findViewById(R.id.question);
+        answer1Input = (EditText) findViewById(R.id.answer1);
+        answer2Input = (EditText) findViewById(R.id.answer2);
+        answer3Input = (EditText) findViewById(R.id.answer3);
+        answer4Input = (EditText) findViewById(R.id.answer4);
+
+
+        publishButton = (Button) findViewById(R.id.publishbutton);
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quiztitle = titleInput.getText().toString();
+            }
+
+        });
+
+        addquestionButton = (Button) findViewById(R.id.addquestionButton);
+        addquestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add a new question
+            }
+
+        });
+
 
         Log.i("Lifecycle","OnCreate() invoked");
 
@@ -39,10 +83,12 @@ public class newquiz_bim extends AppCompatActivity {
        TextView textView3 = findViewById(R.id.log_out);
        textView2.setText("Logout");
 
+        TextView textView4 = findViewById(R.id.create_title);
+        textView2.setText("Create A New Quiz");
 
 
 
-//zvagfiuagfiugaoiufgo
+
        ImageView myImageView = (ImageView) findViewById(R.id.profile);
        myImageView.setImageResource(R.drawable.profilepic);
 
