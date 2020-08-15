4168ed28ca2ba4757bd97a9d1f1c2db977892fd4
UltravioletDarling <iambimsara@gmail.com>
2020-08-15 17:00:36 +0530

    Created New Quiz UI and class

---
+++ app/src/androidTest/java/com/example/madminiproject/newquiz_bim.java
@@ -0,0 +1,17 @@
+package com.example.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.madminiproject.R;
+
+public class newquiz_bim extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newquiz_bim);
+    }
+}
